2144f40b01bb465d520b9521bbbd00b877101c51
Amal <146487055+amal554-arch@users.noreply.github.com>
2026-03-04 14:40:10 +0530

    add getComuputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let choice = parseInt(Math.random() * 10) % 3;
+    
+    if (choice === 0) {
+        return 'Rock';
+    } else if (choice === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors'
+    }
+}
+
+console.log(getComputerChoice())
