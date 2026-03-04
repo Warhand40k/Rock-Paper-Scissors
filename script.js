c927f411370d273ed5a4caf9ac6a04344048992c
Amal <146487055+amal554-arch@users.noreply.github.com>
2026-03-04 15:09:05 +0530

    add the logic to play a single round

--- script.js
+++ script.js
@@ -19,3 +19,52 @@ function getHumanChoice() {
     return choice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    
+    switch (humanChoice) {
+        case 'rock':
+            if(computerChoice === 'paper') {
+                console.log("You lose! Paper beats Rock");
+                computerScore += 1;
+            } else if(computerChoice === 'scissors') {
+                console.log("You win! Rock beats Scissors");
+                humanScore += 1;
+            } else {
+                console.log("Tie! rock does not beat rock");
+            }
+
+            break;
+        
+        case 'paper':
+            if(computerChoice === 'scissors') {
+                console.log("You lose! Scissors beats Paper");
+                computerScore += 1;
+            } else if(computerChoice === 'rock') {
+                console.log("You win! Paper beats Rock");
+                humanScore += 1;
+            } else {
+                console.log("Tie! Paper does not beat Paper");
+            }
+
+            break;
+        
+        case 'scissors':
+            if(computerChoice === 'rock') {
+                console.log("You lose! Rock beats Scissors");
+                computerScore += 1;
+            } else if(computerChoice === 'paper') {
+                console.log("You win! Scissors beats Paper");
+                humanScore += 1;
+            } else {
+                console.log("Tie! Scissors does not beat Scissors");
+            }
+
+            break;
+    }
+
+    console.log(humanScore)
+    console.log(computerScore)
+}
+
+playRound(getHumanChoice(), getComputerChoice())
