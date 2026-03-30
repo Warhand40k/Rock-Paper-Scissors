8d213794835b020ea76361e630da0358aa188004
Amal <146487055+amal554-arch@users.noreply.github.com>
2026-03-31 01:48:29 +0530

    Several Changes:
    add UI
    add fonts
    add icons
    remove the getHumanChoice function
    update README

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Rock-Paper-Scissors
-![Preview](screenshots/screenshot0.png)
-A five-round game of **Rock Paper Scissors** played entirely in the console.
+
+![Preview](screenshots/screenshot1.png)
+
+Implementation of a five-round game of **Rock Paper Scissors** from [The Odin Project](https://www.theodinproject.com/) curriculum.
+
+### Phase 1:
+
+* A simple implementation of the game that is playable only in the browser console.
+
+![screenshot](screenshots/screenshot0.png)
+
+### Phase 2:
+
+* Added UI without changing the core logic of the game.
+* Enjoyed learning DOM and Events.
 
 ## Skills demonstrated
+
+* HTML
+* CSS
 * Javascript
--- script.js
+++ script.js
@@ -1,85 +1,155 @@
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    return (choice === 0) ? 'ROCK':(choice === 1) ? 'PAPER':'SCISSORS';
-}
+const playButton = document.querySelector("#play-button");
 
-function getHumanChoice() {
-    let choice = prompt("Enter your choice: ");
-    return choice;
-}
+playButton.addEventListener('click', () => {
+    const screen = document.querySelector('#screen');
+    const btnArea = document.querySelector("#button-area");
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+    const buttons = document.createElement('div');
+    const rock = document.createElement('button');
+    const paper = document.createElement('button');
+    const scissors = document.createElement('button');
 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toUpperCase();
-        
-        if (humanChoice === computerChoice) {
-            return `It's a tie! ${humanChoice} does not beat ${computerChoice}`;
-        } else {
-            switch (humanChoice) {
-                case 'ROCK':
-                    if(computerChoice === 'PAPER') {
-                        computerScore++;
-                        return "You lose! PAPER beats ROCK";
-                    } else if(computerChoice === 'SCISSORS') {
-                        humanScore++;
-                        return "You win! ROCK beats SCISSORS";
-                    }
+    buttons.setAttribute('id', 'buttons');
 
-                    break;
-                
-                case 'PAPER':
-                    if(computerChoice === 'SCISSORS') {
-                        computerScore++;
-                        return "You lose! SCISSORS beats PAPER";
-                    } else if(computerChoice === 'ROCK') {
-                        humanScore++;
-                        return "You win! PAPER beats ROCK";
-                    }
+    buttons.appendChild(rock);
+    buttons.appendChild(paper);
+    buttons.appendChild(scissors);
 
-                    break;
-                
-                case 'SCISSORS':
-                    if(computerChoice === 'ROCK') {
-                        computerScore++;
-                        return "You lose! ROCK beats SCISSORS";
-                    } else if(computerChoice === 'PAPER') {
-                        humanScore++;
-                        return "You win! SCISSORS beats PAPER";
-                    }
+    const restart = document.createElement('button');
 
-                    break;
+    const rockImg = document.createElement('img');
+    const paperImg = document.createElement('img');
+    const scissorsImg= document.createElement('img');
+    restart.textContent = 'Restart';
+
+    rockImg.setAttribute('src', './assets/icons/hand-rock.svg');
+    rockImg.setAttribute('draggable', 'false');
+    rockImg.style.width = '12vh';
+
+    paperImg.setAttribute('src', './assets/icons/hand-paper.svg');
+    paperImg.setAttribute('draggable', 'false');
+    paperImg.style.width = '12vh';
+
+    scissorsImg.setAttribute('src', './assets/icons/hand-scissors.svg');
+    scissorsImg.setAttribute('draggable', 'false');
+    scissorsImg.style.width = '12vh';
+
+    rock.classList = 'btn';
+    rock.id = 'ROCK';
+    paper.classList = 'btn';
+    scissors.classList = 'btn';
+    restart.classList.add('restart-btn', 'kirang-haerang-regular');
+
+    playButton.remove();
+
+    btnArea.appendChild(buttons);
+    btnArea.appendChild(restart);
+
+    rock.appendChild(rockImg);
+    paper.appendChild(paperImg);
+    scissors.appendChild(scissorsImg);
+
+    function playGame() {
+        const messageArea = document.createElement('div');
+        messageArea.id = 'message-area';
+
+        const message = document.createElement('p');
+        message.textContent = "> Welcome!"
+        message.classList = 'messages';
+
+        let humanScore = 0;
+        let computerScore = 0;
+
+        function handleRockClick() {
+            playRound('ROCK', getComputerChoice());
+        }
+
+        function handlePaperClick() {
+            playRound('PAPER', getComputerChoice());
+        }
+
+        function handleScissorsClick() {
+            playRound('SCISSORS', getComputerChoice());
+        }
+
+        function getComputerChoice() {
+            let choice = Math.floor(Math.random() * 3);
+            return (choice === 0) ? 'ROCK':(choice === 1) ? 'PAPER':'SCISSORS';
+        }
+
+        const displayScore = () => {
+            const score = document.createElement('p');
+            score.textContent = `# You: ${humanScore}  Computer: ${computerScore}`;
+            score.classList = 'score';
+            messageArea.appendChild(score);
+        }
+
+        function playRound(humanChoice, computerChoice) {
+
+            let status = '';
+            if (humanChoice === computerChoice) {
+                status = `It's a tie! ${humanChoice} does not beat ${computerChoice}`;
+            } else {
+                switch (humanChoice) {
+                    case 'ROCK':
+                        if(computerChoice === 'PAPER') {
+                            computerScore++;
+                            status = ("You lose! PAPER beats ROCK");
+                        } else if(computerChoice === 'SCISSORS') {
+                            humanScore++;
+                            status = "You win! ROCK beats SCISSORS";
+                        }
+
+                        break;
+                        
+                    case 'PAPER':
+                        if(computerChoice === 'SCISSORS') {
+                            computerScore++;
+                            status = "You lose! SCISSORS beats PAPER";
+                        } else if(computerChoice === 'ROCK') {
+                            humanScore++;
+                            status = "You win! PAPER beats ROCK";
+                        }
+
+                        break;
+                        
+                    case 'SCISSORS':
+                        if(computerChoice === 'ROCK') {
+                            computerScore++;
+                            status = "You lose! ROCK beats SCISSORS";
+                        } else if(computerChoice === 'PAPER') {
+                            humanScore++;
+                            status = "You win! SCISSORS beats PAPER";
+                        }
+                }
+            }
+            const newMessage = document.createElement('p');
+            newMessage.textContent = `> ${status}`;
+            newMessage.classList = 'messages';
+            messageArea.appendChild(newMessage);
+            displayScore();
+            if (humanScore === 5 || computerScore === 5) {
+                const winner = document.createElement('p');
+                winner.textContent = (humanScore === 5) ? "> *** YOU WIN! ***" : "> *** YOU LOSE! ***";
+                winner.classList = 'messages';
+                messageArea.appendChild(winner);
+                rock.removeEventListener('click', handleRockClick);
+                paper.removeEventListener('click', handlePaperClick);
+                scissors.removeEventListener('click', handleScissorsClick);
             }
         }
+
+        screen.appendChild(messageArea);
+        messageArea.appendChild(message);
+    
+        rock.addEventListener('click', handleRockClick);
+        paper.addEventListener('click', handlePaperClick);
+        scissors.addEventListener('click', handleScissorsClick);
+        restart.addEventListener('click', () => {
+            messageArea.remove();
+            playGame();
+        }, {once: true});
     }
 
-    const btn = document.querySelectorAll('button');
-    const container = document.querySelector('div');
-    const status = document.createElement('p');
-    container.appendChild(status);
-    const score = document.createElement('p');
-    container.appendChild(score);
-    const winner = document.createElement('p');
-    container.appendChild(winner);
-
-    let displayResults = (event) => {
-        const selection = event.target.id;
-        status.textContent = playRound(selection, getComputerChoice());
-        score.textContent = `Your score: ${humanScore} Computer score: ${computerScore}`;
-        if(humanScore === 5 || computerScore === 5) {
-            btn.forEach((button) => {
-                button.removeEventListener('click', displayResults);
-            });
-            status.remove();
-            winner.textContent = (humanScore === 5) ? 'You win!' : 'You lose!';
-        }
-    }
-
-    btn.forEach((button) => {
-        button.addEventListener('click', displayResults);
-    });
-}
-
-playGame();
+    playGame();
+})
